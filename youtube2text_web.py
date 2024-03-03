82520a1f4390914bd2b1d1bddb6aaae25238bfff
Mohammad Shojaei <shojaeireal@gmail.com>
2024-03-03 14:43:29 +0330

    Update youtube2text_web.py

--- youtube2text_web.py
+++ youtube2text_web.py
@@ -76,7 +76,7 @@ def structure_with_ai(transcript_text: str) -> str:
               '''
     response = g4f.ChatCompletion.create(
         model= "",
-        provider=g4f.Provider.FreeChatgpt,
+        provider=g4f.Provider.Koala,
         messages=[
             {"role": "user", "content": request}
         ]

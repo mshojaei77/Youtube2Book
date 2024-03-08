39b24eb2f9e48c95573570cad51380d51eb8379a
Mohammad Shojaei <shojaeireal@gmail.com>
2024-03-08 07:13:52 +0330

    Update youtube2text_web.py

--- youtube2text_web.py
+++ youtube2text_web.py
@@ -4,6 +4,8 @@ from youtube_transcript_api.formatters import TextFormatter
 from yt_dlp import YoutubeDL
 import re
 import g4f
+import time
+from requests.exceptions import HTTPError
 
 
 # Include the Vazirmatn font via CDN
@@ -63,7 +65,7 @@ def get_video_info(video_url: str) -> tuple:
 
 def structure_with_ai(transcript_text: str) -> str:
     request = f'''
-        To transform the provided YouTube video transcript and description into a structured, engaging, and educational format using Markdown, with a strong emphasis on enhancing readability, engagement, and educational value through the strategic use of emojis and tables, follow these instructions:
+        transform the provided YouTube video transcript and description into a structured, engaging, and educational format using Markdown, with a strong emphasis on enhancing readability, engagement, and educational value through the strategic use of emojis and tables:
 
         ### Video Description:
 
@@ -76,55 +78,62 @@ def structure_with_ai(transcript_text: str) -> str:
         {transcript_text}
         ```
 
-        ### Transformation Instructions:
 
-        1. **Structure and Tone**: Organize the content to align with the video's subject matter, ensuring clarity and engagement. Utilize emojis, tables, and code blocks where appropriate to improve the presentation and make the content more dynamic. 📝🚀
+        - organize the content to align with the video's subject matter, ensuring clarity and engagement. Utilize emojis, tables, and code blocks where appropriate to improve the presentation and make the content more dynamic. 📝🚀
 
-        2. **Preservation of Meaning**: Ensure the original content's essence is preserved during the transformation. Avoid omitting or truncating important information. 📖🔍
+        - Ensure the original content's essence is preserved during the transformation. Avoid omitting or truncating important information. 📖🔍
 
-        3. **Incorporation of Links**: Embed URLs from the video description as clickable links within the Markdown document in right place (related to section). Verify all links are functional and correctly formatted. 🌐🔗
+        - Embed URLs from the video description as clickable links within the Markdown document in right place (related to section). Verify all links are functional and correctly formatted. 🌐🔗
 
-        4. **Engagement Elements**: Utilize tables and code blocks to highlight key points and illustrate examples. This strategy will help maintain audience interest and facilitate comprehension. 📊💡
-
-        5. **Review and Feedback**: Upon completion, review the transformed content to ensure it meets the specified criteria and provide feedback for any necessary modifications. 🔄📢
-
-        Please submit the final, transformed content in Markdown format, adhering strictly to the guidelines outlined. The aim is to produce a comprehensive, engaging, and educational document that effectively communicates the video's content.
-
-        Additionally, ensure to correct and complete the transcript text if it contains errors, without omitting or altering important information. You may add code examples and additional context as long as it does not compromise the original meaning of the content.
-
+       - Utilize tables and code blocks to highlight key points and illustrate examples. This strategy will help maintain audience interest and facilitate comprehension. 📊💡
         - **Tables**: Tables are crucial for organizing information in a structured manner, making it easier for the audience to digest and understand the content. Use tables to highlight key points, list steps, or compare different aspects of the video's content. Tables can significantly enhance the readability and engagement of the content by breaking down complex information into digestible chunks [0].
 
-        By integrating emojis and tables effectively, the transformed content will not only be more engaging but also more accessible to a wider audience, ensuring a positive learning experience.
-            
+        Additionally, ensure to correct and complete the transcript text if it contains grammar issues or anything wrong, without omitting or altering important information. You may add examples and additional context from your own knowledge.            
     '''
 
-    # Define the list of providers
     providers = [g4f.Provider.FreeChatgpt, g4f.Provider.Liaobots, g4f.Provider.Koala, g4f.Provider.Llama2, g4f.Provider.ChatForAi]
 
+    max_retries = 3
+    backoff_factor = 2
+
     for prv in providers:
-        try:
-            response = g4f.ChatCompletion.create(
-                model=g4f.models.gpt_4,
-                provider=prv,
-                messages=[
-                    {"role": "user", "content": request}
-                ]
-            )
-            messages = "".join(response)
-            return messages  # Return the response if successful
-        except Exception as e:
-            st.warning(f"Error with provider {prv}: {e}")
-            continue  # Try the next provider
+        retries = 0
+        while retries < max_retries:
+            try:
+                response = g4f.ChatCompletion.create(
+                    model=g4f.models.gpt_4,
+                    provider=prv,
+                    messages=[
+                        {"role": "user", "content": request}
+                    ]
+                )
+                messages = "".join(response)
+                return messages  # Return the response if successful
+            except HTTPError as http_err:
+                if http_err.response.status_code >= 500:  # Retry only on server errors
+                    retries += 1
+                    time.sleep((backoff_factor ** retries) / 10)
+                    st.warning(f"Retry {retries}/{max_retries} for provider {prv} due to server error.")
+                else:
+                    break  # Don't retry on client errors
+            except Exception as e:
+                st.warning(f"Error with provider {prv}: {e}")
+                break  # Break and try the next provider
+        else:
+            st.error(f"Provider {prv} failed after {max_retries} retries.")
 
     # If all providers fail, display an error message
     st.error("Failed to structure the transcript with all providers.")
     return None
+
 
 video_url_input = st.text_input("Enter YouTube Video URL", "")
 structure_with_ai_checkbox = st.checkbox("✨ Structure to Markdown format with AI")
 translate_to_persian_checkbox = st.checkbox("🌏 Extract Persian Transcript")
 submit_button = st.button("Extract Transcript")
 
+
+    
 if submit_button and video_url_input:
     video_id = extract_video_id(video_url_input)
 
@@ -141,6 +150,13 @@ if submit_button and video_url_input:
             if translate_to_persian_checkbox and transcript_text:
                 with st.spinner('Translating to Persian...'):
                     translated_text = translate_to_persian(video_id)
+                    translated_bytes = translated_text.encode('utf-8')
+                    st.download_button(
+                        label="Download Translation",
+                        data=translated_bytes,
+                        file_name='Translation.txt',
+                        mime='text/plain',
+                    )
                     st.markdown(f'''
                     <div dir="rtl" style="text-align: right; font-family: 'Vazirmatn', sans-serif;">
                         <p style="margin-bottom: 0.5rem;"> </p>
@@ -149,23 +165,39 @@ if submit_button and video_url_input:
                         </article>
                     </div>
                     ''', unsafe_allow_html=True)
+                                    
+
 
             if structure_with_ai_checkbox and transcript_text:
                 with st.spinner('Structuring Using AI... (may take a while)'):
                     structured_transcript = structure_with_ai(transcript_text)
+                    structured_bytes = structured_transcript.encode('utf-8')
+                    st.download_button(
+                        label="Download Markdown",
+                        data=structured_bytes,
+                        file_name='Markdown.txt',
+                        mime='text/plain',
+                    )
                     st.markdown(structured_transcript)
             else:
                 if video_title: st.markdown(f"## {video_title}")
                 st.markdown(f" {transcript_text} ")
-            
+                        
             with st.sidebar:
+                transcript_bytes = transcript_text.encode('utf-8')
+                st.download_button(
+                        label="Download Transcript",
+                        data=transcript_bytes,
+                        file_name='Transcript.txt',
+                        mime='text/pdf',
+                )
                 st.markdown(f"## Video Information: ")
                 if video_title: st.markdown(f"### [{video_title}]({video_url})")
                 if video_thumbnail: st.image(video_thumbnail, use_column_width=True)
                 if video_description: st.markdown(f" {video_description}")
 
+
         except Exception as e:
             st.error(f"An error occurred: {e}")
     else:
         st.error("Please enter a valid YouTube video URL.")
-
